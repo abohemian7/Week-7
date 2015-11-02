6ee35483ca0f090947df171cabbb3bdc7d822e52
Andrew Boyer <boyer.andrew.7@gmail.com>
2015-11-01 20:02:27 -0500

    confused

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,2 @@
-$(document).ready(function(){
-  var fizzbuzz = 0;
-  $('#plus-btn').click(function(){
-    fizzbuzz++;
-    $('#fizzbuzz').html(fizzbuzz);
-  });
-  $('#fizzbuzz').html(fizzbuzz);
-});
+
+angular.module('fizzbuzzer',[]).controller('MyController', function(){});
